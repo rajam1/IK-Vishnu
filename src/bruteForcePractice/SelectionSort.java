a020ddf87d7a78595407ced2b7e31c713b6281c1
rajamav <Vishnugopal.Rajamanickam@dematic.com>
2023-02-17 08:22:51 -0600

    Initial commit

---
+++ src/bruteForcePractice/SelectionSort.java
@@ -0,0 +1,16 @@
+package bruteForcePractice;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] input = {1, 3, 5, 2, 3, 7};
+        int[] output = selectionSort(input);
+    }
+
+    private static int[] selectionSort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            // incomplete, will be added later
+        }
+        return a;
+    }
+
+}
