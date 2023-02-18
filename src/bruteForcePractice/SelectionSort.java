86ea18a2e7aa404cf23358c003be434d649301e9
rajamav <Vishnugopal.Rajamanickam@dematic.com>
2023-02-17 21:12:57 -0600

    SelectionSort solved

--- src/bruteForcePractice/SelectionSort.java
+++ src/bruteForcePractice/SelectionSort.java
@@ -2,13 +2,28 @@ package bruteForcePractice;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] input = {1, 3, 5, 2, 3, 7};
+        int[] input = {12, 22, 3, 45, 5, 22};
         int[] output = selectionSort(input);
+        for (int var : output) {
+            System.out.print(var + " ");
+        }
     }
 
     private static int[] selectionSort(int[] a) {
         for (int i = 0; i < a.length; i++) {
-            // incomplete, will be added later
+            int min = a[i];
+            int minIndex = i;
+            for (int k = i + 1; k < a.length; k++) {
+                if (a[k] < min) {
+                    min = a[k];
+                    minIndex = k;
+                }
+            }
+            if (i != minIndex) {
+                int temp = a[i];
+                a[i] = a[minIndex];
+                a[minIndex] = temp;
+            }
         }
         return a;
     }
