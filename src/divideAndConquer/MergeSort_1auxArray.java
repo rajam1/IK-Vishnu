5f8c0d517ccb82d25cf6d0f9bca3b745017e66cb
rajamav <Vishnugopal.Rajamanickam@dematic.com>
2023-03-02 07:34:14 -0600

    Merge Sort updated

--- src/divideAndConquer/MergeSort_1auxArray.java
+++ src/divideAndConquer/MergeSort_1auxArray.java
@@ -2,59 +2,66 @@ package divideAndConquer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
-public class MergeSort {
+public class MergeSort_1auxArray {
+//    public static ArrayList<Integer> aux;
+
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(5, 8, 3, 9, 4, 1, 7));
+//        aux = new ArrayList<Integer>(Collections.nCopies(arr.size(), 0));
         ArrayList<Integer> output = merge_sort(arr);
         System.out.println(output);
     }
 
-    private static ArrayList<Integer> merge_sort(ArrayList<Integer> arr) {
-        helper(arr, 0, arr.size() - 1);
+    static ArrayList<Integer> merge_sort(ArrayList<Integer> arr) {
+        ArrayList<Integer> aux = new ArrayList<Integer>(Collections.nCopies(arr.size(), 0));
+        helper(arr, aux, 0, arr.size() - 1);
         return arr;
     }
 
-    private static ArrayList<Integer> helper(ArrayList<Integer> arr, int start, int end) {
+    private static void helper(ArrayList<Integer> arr, ArrayList<Integer> aux, int start, int end) {
         // leaf node and exit criteria
         if (start == end) {
-            return arr;
+            return;
         }
         // worker or arbitrary worker node
         // First step is to delegate
         int mid = (start + end) / 2;
-        helper(arr, start, mid);
-        helper(arr, mid + 1, end);
+        helper(arr, aux, start, mid);
+        helper(arr, aux, mid + 1, end);
         //Merge sorted halfs
         int i = start;
         int j = mid + 1;
-        ArrayList<Integer> aux = new ArrayList<>();
+        int k = start;
+
         while (i <= mid && j <= end) {
             if (arr.get(i) <= arr.get(j)) {
-                aux.add(arr.get(i));
+                aux.set(k, arr.get(i));
+                k++;
                 i++;
             }
             if (arr.get(j) < arr.get(i)) {
-                aux.add(arr.get(j));
+                aux.set(k, arr.get(j));
+                k++;
                 j++;
             }
         }
         //gather phase - one of the segments is exhausted, so we copy over the remaining from other segment
         while (i <= mid) {
-            aux.add(arr.get(i));
+            aux.set(k, arr.get(i));
+            k++;
             i++;
         }
         while (j <= end) {
-            aux.add(arr.get(j));
+            aux.set(k, arr.get(j));
+            k++;
             j++;
         }
-        //copy contents of aux back to arr in the right place. Aux is going to be smaller than arr.
-        int a = 0;//for aux iteration
-        while (a<aux.size()) {
-            arr.set(start, aux.get(a));
+        while (start <= end) {
+            arr.set(start, aux.get(start));
             start++;
-            a++;
         }
-        return arr;
+        return;
     }
 }
